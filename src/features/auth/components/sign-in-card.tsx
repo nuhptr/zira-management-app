76728d91309025700466278b552229cb7f35b9f1
nuhptr <nugrahaadi733@gmail.com>
2025-10-18 23:52:47 +0700

    01: auth-design update card sign-in/up

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,49 +1,88 @@
+import Link from "next/link"
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Label } from "@/components/ui/label"
+
+const signInFormSchema = z.object({
+  email: z.email().trim(),
+  password: z.string().min(8, "Password must be at least 8 characters.").max(256),
+})
+type SignInFormValues = z.infer<typeof signInFormSchema>
 
 export const SignInCard = () => {
+  const form = useForm<SignInFormValues>({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: { email: "", password: "" },
+  })
+
+  const submitEvent = (data: SignInFormValues) => {
+    console.log({ data })
+  }
+
   return (
-    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader className="flex items-center justify-center text-center p-7">
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none tracking-wide">
+      <CardHeader className="flex items-center justify-center text-center px-7 py-6">
         <CardTitle className="text-2xl">Welcome back!</CardTitle>
       </CardHeader>
+
       <div className="px-7">
         <Separator isDotted />
       </div>
-      <CardContent className="p-7">
-        <form className="space-y-4">
-          <Input
-            required
-            type="email"
-            value={""}
-            onClick={() => {}}
-            placeholder="Enter email address"
-            disabled={false}
-          />
-          <Input
-            required
-            type="password"
-            value={""}
-            onClick={() => {}}
-            placeholder="Enter your password"
-            disabled={false}
-            min={8}
-            max={256}
-          />
-          <Button disabled={false} size={"lg"} className="w-full">
-            Login
-          </Button>
-        </form>
+
+      <CardContent className="px-7 py-6">
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(submitEvent)}>
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col gap-y-2">
+                      <Label>Email</Label>
+                      <Input {...field} type="email" placeholder="Enter email address" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col gap-y-2">
+                      <Label>Password</Label>
+                      <Input {...field} type="password" placeholder="Enter your password" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={false} size={"lg"} className="w-full cursor-pointer">
+              Login
+            </Button>
+          </form>
+        </Form>
       </CardContent>
+
       <div className="px-7">
         <Separator isDotted />
       </div>
-      <CardContent className="p-7 flex flex-col gap-y-4">
+
+      <CardContent className="px-7 py-6 flex flex-col gap-y-4">
         <Button variant={"secondary"} size={"lg"} className="w-full" disabled={false}>
           <FcGoogle className="mr-2 size-5" />
           Login with Google
@@ -52,6 +91,13 @@ export const SignInCard = () => {
           <FaGithub className="mr-2 size-5" />
           Login with Github
         </Button>
+
+        <p className="w-full text-center mt-4">
+          Don't have an account?{" "}
+          <Link href="/sign-up" className="text-blue-700 hover:underline">
+            Sign Up
+          </Link>
+        </p>
       </CardContent>
     </Card>
   )
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,54 +1,116 @@
+import Link from "next/link"
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Label } from "@/components/ui/label"
+
+const signUpFormSchema = z.object({
+  name: z.string().trim().min(1, "Name is required").max(100),
+  email: z.email().trim(),
+  password: z.string().min(8, "Password must be at least 8 characters.").max(256),
+})
+type SignUpFormValues = z.infer<typeof signUpFormSchema>
 
 export const SignUpCard = () => {
+  const form = useForm<SignUpFormValues>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: { name: "", email: "", password: "" },
+  })
+
+  const submitEvent = (data: SignUpFormValues) => {
+    console.log({ data })
+  }
+
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader className="flex flex-col items-center justify-center text-center p-7">
+      <CardHeader className="flex flex-col items-center justify-center text-center px-7 py-5">
         <CardTitle className="text-2xl">
           New in Zira? Register to becoming <br /> part of us!
         </CardTitle>
         <CardDescription>
-          By signing up, you agree to our Terms of Service and Privacy Policy.
+          By signing up, you agree to our Terms of Service and{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700 hover:underline">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700 hover:underline">Terms of Service</span>
+          </Link>
         </CardDescription>
       </CardHeader>
+
       <div className="px-7">
         <Separator isDotted />
       </div>
-      <CardContent className="p-7">
-        <form className="space-y-4">
-          <Input
-            required
-            type="email"
-            value={""}
-            onClick={() => {}}
-            placeholder="Enter email address"
-            disabled={false}
-          />
-          <Input
-            required
-            type="password"
-            value={""}
-            onClick={() => {}}
-            placeholder="Enter your password"
-            disabled={false}
-            min={8}
-            max={256}
-          />
-          <Button disabled={false} size={"lg"} className="w-full">
-            Login
-          </Button>
-        </form>
+
+      <CardContent className="px-7 py-5">
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(submitEvent)}>
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col gap-y-2">
+                      <Label>Full Name</Label>
+                      <Input {...field} type="text" placeholder="Enter your name" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col gap-y-2">
+                      <Label>Email</Label>
+                      <Input {...field} type="email" placeholder="Enter email address" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col gap-y-2">
+                      <Label>Password</Label>
+                      <Input {...field} type="password" placeholder="Enter your password" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={false} size={"lg"} className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
       </CardContent>
+
       <div className="px-7">
         <Separator isDotted />
       </div>
-      <CardContent className="p-7 flex flex-col gap-y-4">
+
+      <CardContent className="px-7 py-5 flex flex-col gap-y-4">
         <Button variant={"secondary"} size={"lg"} className="w-full" disabled={false}>
           <FcGoogle className="mr-2 size-5" />
           Login with Google
@@ -57,6 +119,12 @@ export const SignUpCard = () => {
           <FaGithub className="mr-2 size-5" />
           Login with Github
         </Button>
+        <p className="w-full text-center mt-4">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="text-blue-700 hover:underline">
+            Sign In
+          </Link>
+        </p>
       </CardContent>
     </Card>
   )
